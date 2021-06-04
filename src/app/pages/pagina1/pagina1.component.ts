f02f6ef681d16fe903a4b21c821f23252bde3043
Theo wald <theo83wald@gmail.com>
2021-06-04 09:12:01 +0200

    fin lifecycle

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -8,6 +9,10 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterContentInit, 
     AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  nombre: string = 'Juan Lu';
+  segundos: number = 0;
+  timerSubscription!: Subscription;
 
   constructor() { 
     console.log('constructor');
@@ -33,11 +38,20 @@ export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterConten
   }
   ngOnDestroy(): void {
     console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+    console.log('timer limpiado');
   }
 
   ngOnInit(): void {
     console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe( i => {
+      console.log(i);
+      this.segundos = i;;
+    });
   }
 
+  guardar(){
+
+  }
 
 }
